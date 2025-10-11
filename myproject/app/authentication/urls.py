41077e9e3a451331bdbfe776933023f82d3f9e11
Mihir Wrappixel <mihir.wrappixel@gmail.com>
2025-10-11 17:07:27 +0530

    change in files and folders

--- myproject/app/authentication/urls.py
+++
@@ -1,7 +0,0 @@
-from . import views
-from django_distill import distill_path
-
-urlpatterns = [
-    distill_path('login/', views.login, name='login'),
-    distill_path('register/', views.register, name='register'),
-]
--- myproject/app/authentication/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-def login(request):
-    return render(request, "authentication-login.html")
-
-def register(request):
-    return render(request, "authentication-register.html")
